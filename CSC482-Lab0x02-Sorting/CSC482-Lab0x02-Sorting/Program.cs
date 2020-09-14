43a85a0a45c2c6e20f385cc9d5b2f7d947715cf8
Baruch Oltman <baruch.oltman@outlook.com>
2020-09-14 18:31:00 -0500

    Creates project, initial commit

---
+++ CSC482-Lab0x02-Sorting/CSC482-Lab0x02-Sorting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSC482_Lab0x02_Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
