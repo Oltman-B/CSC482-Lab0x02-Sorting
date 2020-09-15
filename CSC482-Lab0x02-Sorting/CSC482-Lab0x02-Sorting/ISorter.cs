67ed2d8385aebf27fddf11a2e651f97b5ebe54ed
Baruch Oltman <42524526+Oltman-B@users.noreply.github.com>
2020-09-14 21:26:48 -0500

    Changes ISorter so that it can sort any generic IComparable<T> type.

---
+++ CSC482-Lab0x02-Sorting/CSC482-Lab0x02-Sorting/ISorter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSC482_Lab0x02_Sorting
+{
+    // ISorter can only be implemented by types that implement IComparable<T>
+    interface ISorter<T> where T : IComparable<T>
+    {
+        void Sort(List<T> keys);
+    }
+}
