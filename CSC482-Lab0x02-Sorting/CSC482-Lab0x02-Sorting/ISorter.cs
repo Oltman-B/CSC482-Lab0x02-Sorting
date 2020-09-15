18d15c6d1ac793cdb39a3bbf3bf3b612caaf0f3b
Baruch Oltman <42524526+Oltman-B@users.noreply.github.com>
2020-09-14 21:29:56 -0500

    Changes Sort to take an IComparable<T>

--- CSC482-Lab0x02-Sorting/CSC482-Lab0x02-Sorting/ISorter.cs
+++ CSC482-Lab0x02-Sorting/CSC482-Lab0x02-Sorting/ISorter.cs
@@ -7,6 +7,6 @@ namespace CSC482_Lab0x02_Sorting
     // ISorter can only be implemented by types that implement IComparable<T>
     interface ISorter<T> where T : IComparable<T>
     {
-        void Sort(List<T> keys);
+        void Sort(IComparable<T> keys);
     }
 }
