01d6f67b88044efa6bc09de0be819a032c245ea7
Baruch Oltman <42524526+Oltman-B@users.noreply.github.com>
2020-09-19 21:43:28 -0500

    Completes the SortSandbox so that all of the sort types are graphed and statistics are calcualted for doubling lists. The lab is complete.

--- CSC482-Lab0x02-Sorting/CSC482-Lab0x02-Sorting/SortSandbox.cs
+++ CSC482-Lab0x02-Sorting/CSC482-Lab0x02-Sorting/SortSandbox.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using CSC482_Lab0x02_Sorting.Sorts;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Text;
-using CSC482_Lab0x02_Sorting.Sorts;
 
 namespace CSC482_Lab0x02_Sorting
 {
@@ -21,159 +20,184 @@ namespace CSC482_Lab0x02_Sorting
             ActualDoublingRatio = 0;
         }
     }
+
     class SortSandbox
     {
         private readonly List<int> _sortedValues = new List<int>();
 
-            private const int MaxSecondsPerAlgorithm = 1;
-            private const long MaxMicroSecondsPerAlg = MaxSecondsPerAlgorithm * 1000000;
+        private const double MaxSecondsPerAlgorithm = 25;
+        private const double MaxMicroSecondsPerAlg = MaxSecondsPerAlgorithm * 1000000;
 
-            private const int NMin = 1;
-            private const int NMax = int.MaxValue;
+        private const int NMin = 1;
+        private const int NMax = int.MaxValue;
 
-            private readonly Random _rand = new Random();
-            private readonly Stopwatch _stopwatch = new Stopwatch();
+        private readonly Random _rand = new Random();
+        private readonly Stopwatch _stopwatch = new Stopwatch();
 
 
-            private TestData GenerateTestList(int length, int keyWidthInBytes, char minVal, char maxVal)
+        private TestData GenerateTestList(int length, int keyWidthInBytes, char minVal, char maxVal)
+        {
+            var data = new TestData(keyWidthInBytes, length, minVal, maxVal);
+            return data;
+        }
+
+        private TestData GenerateTestList(int length, int keyWidthInBytes)
+        {
+            var data = new TestData(keyWidthInBytes, length);
+            return data;
+        }
+
+        public bool VerificationTests()
+        {
+            var sorters = new List<iSorter<Key>>
             {
-                var data = new TestData(keyWidthInBytes, length, minVal, maxVal);
-                return data;
+                new Quick(), new Merge(), new Selection(),
+                new Radix(RadixSortBase.Base256), new Radix(RadixSortBase.Base65536),
+                new Radix(RadixSortBase.Base16777216)
+            };
+
+            VisualVerificationTests(sorters);
+            if (!NonVisualVerificationTests(sorters))
+            {
+                Console.WriteLine("One or more non-visual tests have failed");
             }
 
-            private TestData GenerateTestList(int length, int keyWidthInBytes)
-            {
-                var data = new TestData(keyWidthInBytes, length);
-                return data;
-            }
 
-            public bool VerificationTests()
-            {
-                var sorters = new List<iSorter<Key>> { new Quick(), new Merge(), new Selection(),
-                    new Radix(RadixSortBase.Base256), new Radix(RadixSortBase.Base65536), new Radix(RadixSortBase.Base16777216) };
+            // All tests pass
+            return true;
+        }
 
-                VisualVerificationTests(sorters);
-                if (!NonVisualVerificationTests(sorters))
+        private void VisualVerificationTests(List<iSorter<Key>> sorters)
+        {
+            foreach (var sorter in sorters)
+            {
+                var test = GenerateTestList(5, 10, 'a', 'z');
+
+                Console.WriteLine($"Testing sorter {sorter.GetType()}");
+                foreach (var testKey in test.Keys)
                 {
-                    Console.WriteLine("One or more non-visual tests have failed");
+                    Console.Write(testKey.ToString() + " ");
                 }
 
+                Console.WriteLine(" Sorted? " + test.IsSorted());
 
-                // All tests pass
-                return true;
-            }
-
-            private void VisualVerificationTests(List<iSorter<Key>> sorters)
-            {
-                foreach (var sorter in sorters)
+                Console.WriteLine("Now Sorting...");
+                sorter.Sort(test.Keys);
+                foreach (var testKey in test.Keys)
                 {
-                    var test = GenerateTestList(5, 10, 'a', 'z');
-                    
-                    Console.WriteLine($"Testing sorter {sorter.GetType()}");
-                    foreach (var testKey in test.Keys)
-                    {
-                        Console.Write(testKey.ToString() + " ");
-                    }
-
-                    Console.WriteLine(" Sorted? " + test.IsSorted());
-
-                    Console.WriteLine("Now Sorting...");
-                    sorter.Sort(test.Keys);
-                    foreach (var testKey in test.Keys)
-                    {
-                        Console.Write(testKey.ToString() + " ");
-                    }
-
-                    Console.Write(" Sorted? " + test.IsSorted());
-                    Console.WriteLine("\n");
+                    Console.Write(testKey.ToString() + " ");
                 }
-            }
 
-            private bool NonVisualVerificationTests(List<iSorter<Key>> sorters)
+                Console.Write(" Sorted? " + test.IsSorted());
+                Console.WriteLine("\n");
+            }
+        }
+
+        private bool NonVisualVerificationTests(List<iSorter<Key>> sorters)
+        {
+            bool allTestsPassed = true;
+            foreach (var sorter in sorters)
             {
-                bool allTestsPassed = true;
-                foreach (var sorter in sorters)
+                Console.WriteLine("Generating list of length 100000, key width of 24");
+                var test = GenerateTestList(10000, 24);
+
+                Console.WriteLine($"Sorting with {sorter.GetType()}");
+                sorter.Sort(test.Keys);
+
+                if (test.IsSorted())
                 {
-                    Console.WriteLine("Generating list of length 100000, key width of 24");
-                    var test = GenerateTestList(10000, 24);
-
-                    Console.WriteLine($"Sorting with {sorter.GetType()}");
-                    sorter.Sort(test.Keys);
-
-                    if (test.IsSorted())
-                    {
-                        Console.WriteLine("Verifying... Sorted!");
-                    }
-                    else
-                    {
-                        allTestsPassed = false;
-                        Console.WriteLine("Verifying... Not Sorted!");
-                    }
-                    
-                    Console.WriteLine("\n");
+                    Console.WriteLine("Verifying... Sorted!");
                 }
-                return allTestsPassed;
+                else
+                {
+                    allTestsPassed = false;
+                    Console.WriteLine("Verifying... Not Sorted!");
+                }
+
+                Console.WriteLine("\n");
             }
 
+            return allTestsPassed;
+        }
 
-            public void RunTimeTests()
+
+        public void RunTimeTests()
+        {
+            var sorters = new List<iSorter<Key>>
             {
-                PrintHeader();
+                new Quick(), new Merge(), new Selection(),
+                new Radix(RadixSortBase.Base256), new Radix(RadixSortBase.Base65536),
+                new Radix(RadixSortBase.Base16777216)
+            };
 
-                var currentStats = new SortStats();
+            foreach (var sorter in sorters)
+            {
+                SortTestRuntime(sorter);
+            }
+        }
 
-                for (var n = NMin; n * 2 <= NMax; n *= 2)
-                    if (currentStats.PrevTimeMicro < MaxMicroSecondsPerAlg)
-                    {
-                        PrintIndexColumn(n);
-                        for (var k = 6; k <= 48; k *= 2)
-                        {
-                            var testData = GenerateTestList(n, k);
-                            _stopwatch.Restart();
-                            SelectionSortRunTimeTests(testData.Keys);
-                            _stopwatch.Stop();
+        private void SortTestRuntime(iSorter<Key> sorter)
+        {
+            PrintHeader(sorter);
 
-                            currentStats.PrevTimeMicro = currentStats.TimeMicro;
-                            currentStats.TimeMicro = TicksToMicroseconds(_stopwatch.ElapsedTicks);
-
-                            CalculateSelectionSortDoublingRatios(n, currentStats);
-
-                            PrintData(n, currentStats);
-
-                            //TODO Problem here, the measuring of actual dobuling ratio is wrong for all k after 6
-                        }
-                        // New Row
-                        Console.WriteLine();
-                    }
+            // This can probably be handled in a better way, but we need to keep track of
+            // the sort stats for each K, so that we can calculate a row and save previous times.
+            const int differentKCount = 4;
+            var currentStats = new List<SortStats>();
+            for (int i = 0; i < differentKCount; i++)
+            {
+                currentStats.Add(new SortStats());
             }
 
-            private void MergeSortRunTimeTests(List<Key> keys)
-        {
-            iSorter<Key> merge = new Merge();
-            SortRunTimeTests(merge, keys);
+            for (var n = NMin; n * 2 < NMax; n *= 2)
+            {
+                // break if largest k value takes longer than allowed time. Check n > 2 to ignore first few
+                // iterations of test, they tend to have a longer wind-up time.
+                if (currentStats[^1].TimeMicro > MaxMicroSecondsPerAlg || n > 1048576)
+                {
+                    PrintSortTerminationMessage(sorter);
+                    break;
+                }
+
+                PrintIndexColumn(n);
+                for (int k = 6, kIndex = 0; k <= 48; kIndex++, k *= 2)
+                {
+                    var testData = GenerateTestList(n, k);
+                    _stopwatch.Restart();
+                    sorter.Sort(testData.Keys);
+                    _stopwatch.Stop();
+
+                    currentStats[kIndex].PrevTimeMicro = currentStats[kIndex].TimeMicro;
+                    currentStats[kIndex].TimeMicro = TicksToMicroseconds(_stopwatch.ElapsedTicks);
+
+                    CalculateDoublingRatios(n, k, currentStats[kIndex], sorter);
+
+                    PrintData(n, currentStats[kIndex]);
+                }
+
+                // New Row
+                Console.WriteLine();
+            }
         }
 
-        private void QuickSortRunTimeTests(List<Key> keys)
+        private void CalculateDoublingRatios(int n, int k, SortStats stats, iSorter<Key> sorter)
         {
-            iSorter<Key> quick = new Quick();
-            SortRunTimeTests(quick, keys);
-        }
-
-        private void SelectionSortRunTimeTests(List<Key> keys)
-        {
-            iSorter<Key> selection = new Selection();
-            SortRunTimeTests(selection, keys);
-        }
-
-        private void RadixSortRunTimeTests(List<Key> keys, RadixSortBase d)
-        {
-            iSorter<Key> radix = new Radix(d);
-            SortRunTimeTests(radix, keys);
-        }
-
-        private void SortRunTimeTests(iSorter<Key> sorter, List<Key> keys)
-        {
-            sorter.Sort(keys);
+            // Switch on sorter type so that appropriate doubling calculation method can be called.
+            switch (sorter)
+            {
+                case Merge m:
+                    CalculateMergeSortDoublingRatios(n, stats);
+                    break;
+                case Quick q:
+                    CalculateQuickSortDoublingRatios(n, stats);
+                    break;
+                case Selection s:
+                    CalculateSelectionSortDoublingRatios(n, stats);
+                    break;
+                case Radix r:
+                    CalculateRadixSortDoublingRatios(n, k, stats);
+                    break;
+            }
         }
 
         private void CalculateMergeSortDoublingRatios(int n, SortStats stats)
@@ -185,7 +209,33 @@ namespace CSC482_Lab0x02_Sorting
                 return;
             }
 
-            stats.ExpectedDoublingRatio = (n*Math.Log2(n)) / (((double)n/2)*(Math.Log2((double)n/2)));
+            stats.ExpectedDoublingRatio = (n * Math.Log2(n)) / (((double)n / 2) * (Math.Log2((double)n / 2)));
+            stats.ActualDoublingRatio = stats.TimeMicro / stats.PrevTimeMicro;
+        }
+
+        private void CalculateQuickSortDoublingRatios(int n, SortStats stats)
+        {
+            if (n <= 2)
+            {
+                stats.ExpectedDoublingRatio = -1;
+                stats.ActualDoublingRatio = -1;
+                return;
+            }
+
+            stats.ExpectedDoublingRatio = (n * Math.Log2(n)) / (((double)n / 2) * (Math.Log2((double)n / 2)));
+            stats.ActualDoublingRatio = stats.TimeMicro / stats.PrevTimeMicro;
+        }
+
+        private void CalculateRadixSortDoublingRatios(int n, int k, SortStats stats)
+        {
+            if (n <= 2)
+            {
+                stats.ExpectedDoublingRatio = -1;
+                stats.ActualDoublingRatio = -1;
+                return;
+            }
+
+            stats.ExpectedDoublingRatio = (n * k) / (((double)n / 2) * ((double)k));
             stats.ActualDoublingRatio = stats.TimeMicro / stats.PrevTimeMicro;
         }
 
@@ -198,16 +248,22 @@ namespace CSC482_Lab0x02_Sorting
                 return;
             }
 
-            stats.ExpectedDoublingRatio = (n*n) / ((double)n/2 * n/2);
+            stats.ExpectedDoublingRatio = (n * n) / ((double)n / 2 * n / 2);
             stats.ActualDoublingRatio = stats.TimeMicro / stats.PrevTimeMicro;
         }
 
-        private void PrintHeader()
+        private void PrintHeader(iSorter<Key> sorter)
         {
+            Console.WriteLine($"Starting run-time tests for {sorter.GetType().Name} sort...\n");
             Console.WriteLine(
-                " \t\t|k=6 ||  Doubling Ratios   ||k=12    Doubling Ratios   ||k=24     Doubling Ratios  ||k=48    Doubling Ratios   |");
+                " \t\t\t        k=6|    |  Doubling Ratios   |          k=12|    |  Doubling Ratios   |          k=24|    |   Doubling Ratios  |          k=48|      Doubling Ratios   |");
             Console.WriteLine(
-                "N\t\t|Time|| Actual || Expected ||Time|| Actual || Expected ||Time|| Actual || Expected ||Time|| Actual || Expected |");
+                "N\t\t\t       Time|    | Actual  | Expected |          Time|    | Actual  | Expected |          Time|    | Actual  | Expected |          Time|    | Actual  | Expected|");
+        }
+
+        private void PrintSortTerminationMessage(iSorter<Key> sorter)
+        {
+            Console.WriteLine($"{sorter.GetType().Name} sort exceeded allotted time, terminating...\n");
         }
 
         private void PrintIndexColumn(int n)
@@ -217,14 +273,14 @@ namespace CSC482_Lab0x02_Sorting
         private void PrintData(int n, SortStats stats)
         {
             var actualDoubleFormatted = stats.ActualDoublingRatio < 0
-                ? "na".PadLeft(14)
-                : stats.ActualDoublingRatio.ToString("F2").PadLeft(14);
+                ? "na".PadLeft(12)
+                : stats.ActualDoublingRatio.ToString("F2").PadLeft(12);
             var expectDoubleFormatted = stats.ExpectedDoublingRatio < 0
-                ? "na".PadLeft(17)
-                : stats.ExpectedDoublingRatio.ToString("F2").PadLeft(17);
+                ? "na".PadLeft(7)
+                : stats.ExpectedDoublingRatio.ToString("F2").PadLeft(7);
 
             Console.Write(
-                $"{stats.TimeMicro,15:F2} {actualDoubleFormatted} {expectDoubleFormatted}");
+                $"{stats.TimeMicro,20:F2} {actualDoubleFormatted} {expectDoubleFormatted}");
         }
 
         private static double TicksToMicroseconds(long ticks)
